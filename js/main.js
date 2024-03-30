cea1020234dbd9b694003380a331b8b1bd85f488
Parth Panara <158038443+PanaraParth@users.noreply.github.com>
2024-03-31 01:37:51 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let image = document.getElementById('#imgcng')[0];
+
+document.getElementsByClassName('svc1')[0].onmouseover = function() {
+  image.src = '../images/nsi2.png';
+  
+};
+
+document.getElementsByClassName('svc2')[0].onmouseover = function() {
+image.src = '../Test/nsi3.png';
+};
+
+document.getElementsByClassName('svc3')[0].onmouseover = function() {
+image.src = '../Test/nsi4.png';
+};
